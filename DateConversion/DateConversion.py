e14c88d6d04f4962d7e887a847228d1df558221f
Nathan Brooks <nathan.brooks@eagles.ewu.edu>
2017-12-04 14:17:45 -0800

    Update DateConversion.py

--- DateConversion/DateConversion.py
+++ DateConversion/DateConversion.py
@@ -3,13 +3,17 @@ import sublime_plugin
 import datetime
 
 class UnixTimeConvert(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, precision):
+        if precision:
+            divisor = 1000
+        else:
+            divisor = 1
+
         for curSel in self.view.sel():
             inputRegion = self.view.expand_by_class(curSel, sublime.CLASS_WORD_START | sublime.CLASS_WORD_END, " ,\"")
             startingPoint = inputRegion.a
             inputStr = self.view.substr(inputRegion)
-            print("input string:" + inputStr)
-            outputStr = datetime.datetime.fromtimestamp(int(inputStr)).strftime('%Y-%m-%d %H:%M:%S')
+            outputStr = datetime.datetime.fromtimestamp(int(inputStr)/ divisor).strftime('%Y-%m-%d %H:%M:%S')
             self.view.erase(edit, inputRegion)
             self.view.insert(edit, startingPoint, outputStr)
 
